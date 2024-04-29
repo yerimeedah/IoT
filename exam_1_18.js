47fe251710f0da8bab664e883f457b501439d856
yerimeedah <dksdpsfla1118@cau.ac.kr>
2024-04-29 14:32:28 +0900

    JSON을 활용한 간단한 깊은 복사 방법을 활용한 코드

---
+++ exam_1_18.js
@@ -0,0 +1,22 @@
+var copy0bjectViaJSON = function(target){
+    return JSON.parse(JSON.stringify(target));
+};
+
+var obj = {
+    a: 1,
+    b: {
+        c:null,
+        d:[1,2],
+        func1:function () {console.log(3);}
+    },
+    func2: function () {console.log(4);}
+};
+var obj2 = copy0bjectViaJSON(obj);
+
+obj2.a = 3;
+obj2.b.c = 4;
+obj.b.d[1] = 3;
+
+console.log(obj);
+console.log(obj2);
+
