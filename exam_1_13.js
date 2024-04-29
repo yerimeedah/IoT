46cacc2edec368163b250ad1b2ff5608cb8eca3f
yerimeedah <dksdpsfla1118@cau.ac.kr>
2024-04-29 14:30:18 +0900

    copyObject을 활용한 객체 복사 관련 코드

---
+++ exam_1_13.js
@@ -0,0 +1,22 @@
+var copy0bject =function (target){
+    var result = {};
+    for(var prop in target){
+        result[prop] = target[prop];
+    }
+    return result;
+};
+
+var user = {
+    name: 'Jaenam',
+    gender:'male'
+};
+
+var user2 = copy0bject(user);
+user2.name = 'Jung';
+
+if(user !== user2){
+    console.log('유저 정보가 변경되었습니다.');
+}
+
+console.log(user.name, user2.name);
+console.log(user === user2);
