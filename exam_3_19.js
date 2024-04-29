05dc6ef8820fea6d68cc207cd7cf38282dec2cc6
yerimeedah <dksdpsfla1118@cau.ac.kr>
2024-04-29 15:18:18 +0900

    문자열에 배열 메서드 적용했을 대 call apply 메서드 활용

---
+++ exam_3_19.js
@@ -0,0 +1,27 @@
+var str = 'abc def';
+
+Array.prototype.push.call(str, ', pushed string');
+// Error: Cannot assign to read only property 'length' of object [object String]
+
+Array.prototype.concat.call(str, 'string'); // [String {"abc def"}, "string"]
+
+Array.prototype.every.call(str, function(char) {
+  return char !== ' ';
+}); // false
+
+Array.prototype.some.call(str, function(char) {
+  return char === ' ';
+}); // true
+
+var newArr = Array.prototype.map.call(str, function(char) {
+  return char + '!';
+});
+console.log(newArr); // ['a!', 'b!', 'c!', ' !', 'd!', 'e!', 'f!']
+
+var newStr = Array.prototype.reduce.apply(str, [
+  function(string, char, i) {
+    return string + char + i;
+  },
+  '',
+]);
+console.log(newStr); // "a0b1c2 3d4e5f6"
