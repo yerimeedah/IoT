47476248f6991317e23b33648252acd268c26001
yerimeedah <dksdpsfla1118@cau.ac.kr>
2024-05-23 21:45:16 +0900

    eventListner 에 의한 메모리 해제로 , clickhandler 식별자의 함수 참조를 끊음

---
+++ exam_5_5_3.js
@@ -0,0 +1,16 @@
+// (3) eventListener에 의한 클로저의 메모리 해제
+(function () {
+    var count = 0;
+    var button = document.createElement('button');
+    button.innerText = 'click';
+  
+    var clickHandler = function () {
+      console.log(++count, 'times clicked');
+      if (count >= 10) {
+        button.removeEventListener('click', clickHandler);
+        clickHandler = null; // clickHandler 식별자의 함수 참조를 끊음
+      }
+    };
+    button.addEventListener('click', clickHandler);
+    document.body.appendChild(button);
+  })();
