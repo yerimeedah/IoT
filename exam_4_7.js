b7868d83ba6da5e43d7cbd51cb374b7b6e92a477
yerimeedah <dksdpsfla1118@cau.ac.kr>
2024-05-23 20:41:26 +0900

    zhfqo

--- exam_4_7.js
+++
@@ -1,8 +0,0 @@
-var obj = {
-    vals: [1, 2, 3],
-    logValues: function(v, i) {
-      console.log(this, v, i);
-    },
-  };
-  obj.logValues(1, 2); 
-  [4, 5, 6].forEach(obj.logValues); 
--- exam_4_9.js
+++
@@ -1,7 +0,0 @@
-var obj1 = {
-    name: 'obj1',
-    func: function() {
-      console.log(obj1.name);
-    },
-  };
-  setTimeout(obj1.func, 1000);
