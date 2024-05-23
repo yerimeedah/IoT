98007dd267f35a5bf11a9f2ad7e3df5dafedad56
yerimeedah <dksdpsfla1118@cau.ac.kr>
2024-05-23 21:42:58 +0900

    return 없이도 지역변수를 참조하는 내부 함수를 외부에 전달하게 되면 클로저가 발생하게 됨을 보여주는 코드 예지

---
+++ exam_5_4.js
@@ -0,0 +1,10 @@
+// (2) eventListener
+(function () {
+    var count = 0;
+    var button = document.createElement('button');
+    button.innerText = 'click';
+    button.addEventListener('click', function () {
+      console.log(++count, 'times clicked');
+    });
+    document.body.appendChild(button);
+  })();
