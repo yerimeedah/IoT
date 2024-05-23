4c30eab0c1e28b79af10822c0920bd3b7a863e46
yerimeedah <dksdpsfla1118@cau.ac.kr>
2024-05-23 21:43:40 +0900

    이전 예제 5-3,4에 메모리 해제 코드를 추가한 코드

---
+++ exam_5_5.js
@@ -0,0 +1,11 @@
+// (1) return에 의한 클로저의 메모리 해제
+var outer = (function () {
+    var a = 1;
+    var inner = function () {
+      return ++a;
+    };
+    return inner;
+  })();
+  console.log(outer());
+  console.log(outer());
+  outer = null;
