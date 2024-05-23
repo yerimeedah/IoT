65a4d542f2c451aa2f8d941da91272ce469034a3
yerimeedah <dksdpsfla1118@cau.ac.kr>
2024-05-23 21:44:24 +0900

    setInterval에 의한 클로저의 메모리 해제로 inner 식별자의 함수 참조를 끊음

---
+++ exam_5_5_2.js
@@ -0,0 +1,13 @@
+// (2) setInterval에 의한 클로저의 메모리 해제
+(function() {
+    var a = 0;
+    var intervalId = null;
+    var inner = function() {
+      if (++a >= 10) {
+        clearInterval(intervalId);
+        inner = null;
+      }
+      console.log(a);
+    };
+    intervalId = setInterval(inner, 1000);
+  })();
