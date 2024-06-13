fe13ac76b9cc6d2c8a7237560725fed5047d8afb
yerimeedah <dksdpsfla1118@cau.ac.kr>
2024-06-13 11:21:09 +0900

    length 프로퍼티를 삭제한 경우의 코드

---
+++ exam_7_3.js
@@ -0,0 +1,16 @@
+var Grade = function () {
+    var args = Array.prototype.slice.call(arguments);
+    for (var i = 0; i < arguments.length; i++) {
+      this[i] = args[i];
+    }
+    this.length = args.length;
+  };
+  Grade.prototype = [];
+  var g = new Grade(100, 80);
+   
+  g.push(90);
+  console.log(g); // Array { '0': 100, '1': 80, '2': 90, length: 3 }
+  
+  delete g.length;
+  g.push(70);
+  console.log(g); // Array { '0': 70, '1': 80, '2': 90, length: 1 }
