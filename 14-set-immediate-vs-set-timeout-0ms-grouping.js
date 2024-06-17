0a7c20ade9c81702cf47cbe450b1b889ff191286
yerimeedah <dksdpsfla1118@cau.ac.kr>
2024-06-17 18:48:22 +0900

    예제 1, denos ex14

---
+++ 14-set-immediate-vs-set-timeout-0ms-grouping.js
@@ -0,0 +1,18 @@
+const firstSetImmediate = () => console.log('setImmediate 1');
+const secondSetImmediate = () => console.log('setImmediate 2');
+const thirdSetImmediate = () => console.log('setImmediate 3');
+const fourthSetImmediate = () => console.log('setImmediate 4');
+
+const firstSetTimeout = () => console.log('setTimeout 1');
+const secondSetTimeout = () => console.log('setTimeout 2');
+const thirdSetTimeout = () => console.log('setTimeout 3');
+const fourthSetTimeout = () => console.log('setTimeout 4');
+
+setImmediate(firstSetImmediate);
+setTimeout(firstSetTimeout, 0);
+setImmediate(secondSetImmediate);
+setTimeout(secondSetTimeout, 0);
+setTimeout(thirdSetTimeout, 0);
+setImmediate(thirdSetImmediate);
+setImmediate(fourthSetImmediate);
+setTimeout(fourthSetTimeout, 0);
